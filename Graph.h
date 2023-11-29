31600bcb9e9e81ec6c34a9a283924a8cb75ba0ce
lucas <lucasmach@ufl.edu>
2023-11-29 14:28:40 -0500

    push

--- Graph.h
+++ Graph.h
@@ -5,35 +5,32 @@
 #include <iostream>
 using namespace std;
 
-class Star {
-public:
+struct Star {
     string name;
-public:
-    Star(string name) {
+    Star(string name) { //Need to add coordinate system to calculate distances between stars
         this->name = name;
     }
 };
 
-
 class Graph {
-public:
     map<Star*, map<Star*, int>> adjList; //Maps, a star to its other stars, with the weight(distance)
-    map<string, Star*> m;   //maps name of star to corresponding object
-
+    map<string, Star*> m;   //maps name of star to corresponding object, created to easily retrieve object data with specified name
 public:
     void Add(Star* s) {   // Add Star to every other star if the other star has less than 5 star connections
                         // If has more than 5, find the star with the largest distance and replace
-                        // ****STILL NEED TO ACCOUNT FOR ADDING THE FIRST STAR****
-
         adjList[s] = {};
+
         // Iterate through all stars
         for (auto iter = adjList.begin(); iter != adjList.end(); iter++) {
+            //Connection of iterated Star to new star
             if (iter->second.size() < 5 and s->name != iter->first->name) {
                 adjList[iter->first][s] = CalcDistance(iter->first, s);
             }
             else if (iter->second.size() >= 5 and s->name != iter->first->name) {
                 RemoveFurthestStar(iter->first, s);
             }
+
+            //Connection of new star to iterated star
             if (adjList[s].size() < 5 and s->name != iter->first->name) {
                 adjList[s][iter->first] = CalcDistance(s, iter->first);
             }
@@ -42,7 +39,7 @@ public:
             }
         }
 
-        if (m.find(s->name) == m.end()) {    // Is not in Map
+        if (m.find(s->name) == m.end()) {    // Is not in map
             m[s->name] = s;
         }
     }
@@ -72,7 +69,6 @@ public:
             }
             cout <<"^ is for star: " <<iter->first->name <<"-----------------" << endl;
         }
-
     }
 
 
