3f0e021871b9c1904eb6cefccefb0fc131407a4f
lucas <lucasmach@ufl.edu>
2023-12-02 14:44:59 -0500

    new

--- Graph.h
+++ Graph.h
@@ -20,41 +20,13 @@ struct Star {
 };
 
 class Graph {
-    map<Star*, map<Star*, int>> adjList; //Maps, a star to its other stars, with the weight(distance)
+    map<Star*, map<Star*, double>> adjList; //Maps, a star to its other stars, with the weight(distance)
     map<string, Star*> m;   //maps name of star to corresponding object, created to easily retrieve object data with specified name
     vector<Star*> randomFind;
-    int maxConnections = 5; // Max amount of star connections 1 star can have
-public:
-    void Add(Star* s) {   // Add Star to every other star if the other star has less than "MaxConntection" star connections
-                        // If has more than 5, find the star with the largest distance and replace
-        adjList[s] = {};
-        randomFind.push_back(s);
-        // Iterate through all stars
-        for (auto iter = adjList.begin(); iter != adjList.end(); iter++) {
-            //Connection of iterated Star to new star
-            if (iter->second.size() < maxConnections and s->name != iter->first->name) {
-                adjList[iter->first][s] = CalcDistance(iter->first, s);
-            }
-            else if (iter->second.size() >= maxConnections and s->name != iter->first->name) {
-                RemoveFurthestStar(iter->first, s);
-            }
-
-            //Connection of new star to iterated star
-            if (adjList[s].size() < maxConnections and s->name != iter->first->name) {
-                adjList[s][iter->first] = CalcDistance(s, iter->first);
-            }
-            else if (adjList[s].size() >= maxConnections and s->name != iter->first->name){
-                RemoveFurthestStar(s, iter->first);
-            }
-        }
-
-        if (m.find(s->name) == m.end()) {    // Is not in map
-            m[s->name] = s;
-        }
-    }
-
-    float CalcDistance(Star* first, Star* second) {   //Calculate distance between two stars
-
+    int maxConnections = 5; // max amount of star connections 1 star can have
+    int count = 0;
+    double CalcDistance(Star* first, Star* second) {   // Calculate distance between two stars
+        return sqrt(pow((second->x - first->x), 2) + pow((second->y - first->y), 2) + pow((second->z - first->z), 2));
     }
 
     void RemoveFurthestStar(Star* star, Star* newStar) {    // Erase the Star furthest of the 5 connections if passed in star is smaller
@@ -71,14 +43,122 @@ public:
         }
     }
 
+    
+    Star* BFS(Star* start, Star* search) {
+        // implementing a queue for BFS
+        queue<Star*> q;
+
+        // use a set to track visited star
+        set<Star*> visitedStar;
+        // start with specified star
+        q.push(start);
+        visitedStar.insert(start);
+
+        while (!q.empty()) {
+            Star *currStar = q.front();
+            q.pop();
+
+            cout << currStar->name << endl;
+            // check is the current star is one of the closest
+            if (currStar == search) {
+                cout << search->name << endl;
+                return currStar;
+            }
+            // check the adjacent stars
+            for (auto it: adjList[currStar]) {
+                Star *adjacentStar = it.first;
+
+                // if the adjacent list isn't visitied add it to the queue
+                if (visitedStar.find(adjacentStar) == visitedStar.end()) {
+                    q.push(adjacentStar);
+                    visitedStar.insert(adjacentStar);
+                }
+
+
+            }
+        }
+        return nullptr;
+    }
+
+    Star* HelperDFS(Star* currStar, Star* search, set<Star*> &s) {
+        if (s.find(currStar) != s.end()) {  //Star is already in Set
+            return nullptr;
+        }
+        for (auto it = adjList[currStar].begin(); it != adjList[currStar].end(); it++) {
+            if (it->first == search) { return currStar;}    //If star found return star
+            else {  //Else call Recursive function with next
+                s.insert(it->first);
+                HelperDFS(it->first, search, s);
+            }
+        }
+    }
+
+    Star* DFS(Star* start, Star* search) {
+        set<Star*> set = {};
+        return HelperDFS(start, search , set);
+
+    }
+
+
+public:
+    ~Graph() {
+        for (auto &pair : adjList) {
+            delete pair.first;
+        }
+    }
+    Star* SearchRandomStar() {
+        int randint = rand() % count;
+        Star* randStar = randomFind[randint];
+        auto iter = adjList.begin();
+        Star* d = DFS(iter->first, randStar);
+        Star* b = BFS(iter->first, randStar);
+        cout << randStar->name << " " << d->name << " " << b->name << endl;
+        if (d == b) {return d;}
+    }
+
+    void AddStar(Star* s) {   // add Star to every other star if the other star has less than "MaxConnection" star connections
+                        // if has more than "maxConnection", find the star with the largest distance and replace
+        adjList[s] = {};
+        count++;
+        randomFind.push_back(s);
+        // Iterate through all stars
+        for (auto iter = adjList.begin(); iter != adjList.end(); iter++) {
+            //Connection of iterated Star to new star
+            if (iter->second.size() < maxConnections and s->name != iter->first->name) {
+                adjList[iter->first][s] = CalcDistance(iter->first, s);
+            }
+
+            else if (iter->second.size() >= maxConnections and s->name != iter->first->name) {
+                RemoveFurthestStar(iter->first, s);
+            }
+
+            //Connection of new star to iterated star
+            if (adjList[s].size() < maxConnections and s->name != iter->first->name) {
+                adjList[s][iter->first] = CalcDistance(s, iter->first);
+            }
+
+            else if (adjList[s].size() >= maxConnections and s->name != iter->first->name){
+                RemoveFurthestStar(s, iter->first);
+            }
+        }
+
+        //if (m.find(s->name) == m.end()) {    // Is not in map
+        //    m[s->name] = s;
+        //}
+    }
+
+
     void PrintStarConnections() {
         for (auto iter = adjList.begin(); iter != adjList.end(); iter++) {
+
             for (auto it = adjList[iter->first].begin(); it != adjList[iter->first].end(); it++) {
-                cout << it->first->name << endl;
+                cout << it->first->name << " "<< it->second <<endl;
             }
             cout <<"^ is for star: " <<iter->first->name <<"-----------------" << endl;
         }
     }
+
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -15,34 +15,81 @@ using namespace std;
 
 
 int main() {
-    Graph g;
-    Star s("lucas");
-    Star e("bill");
-    Star a("will");
-    Star b("gill");
-    Star n("to");
-    Star m("ll");
-    Star l("lu");
-    Star k("b");
-    Star j("as");
-    Star h("b");
-    Star f("bucas");
-    Star w("mill");
-    g.Add(&s);
-    g.Add(&e);
-    g.Add(&w);
-    g.Add(&f);
-    g.Add(&a);
-    g.Add(&b);
-    g.Add(&n);
-    g.Add(&m);
-    g.Add(&l);
-    g.Add(&k);
-    g.Add(&j);
+    fstream file("../HYGData.csv");
+    string line;
+    Graph starSystem;
+    int starCount = 0;
+    if (file.is_open()) {
+
+        getline(file, line); // skip header line
+        while (getline(file, line)) {
+            if (starCount >= 10000) { // cap star count at 120,000
+                break;
+            }
+
+            istringstream iss(line);
+            string col;
+            int id;
+            string constellation;
+            double distFromSun = 0, x = 0, y = 0, z = 0, colorIndex = 0, radialVelocity = 0, vx = 0, vy = 0, vz = 0;
+
+            try {
+                getline(iss, col, ',');
+                if (!col.empty()) id = stoi(col);
+
+                getline(iss, constellation, ',');
+
+                getline(iss, col, ',');
+                if (!col.empty()) distFromSun = stod(col);
+
+                getline(iss, col, ',');
+                if (!col.empty()) x = stod(col);
+
+                getline(iss, col, ',');
+                if (!col.empty()) y = stod(col);
+
+                getline(iss, col, ',');
+                if (!col.empty()) z = stod(col);
+
+                getline(iss, col, ',');
+                if (!col.empty()) colorIndex = stod(col);
+
+                getline(iss, col, ',');
+                if (!col.empty()) radialVelocity = stod(col);
+
+                getline(iss, col, ',');
+                if (!col.empty()) vx = stod(col);
+
+                getline(iss, col, ',');
+                if (!col.empty()) vy = stod(col);
+
+                getline(iss, col, ',');
+                if (!col.empty()) vz = stod(col);
+
+                if (id == 1 or (x != 0 and y != 0 and z != 0)) {
+                    Star *star = new Star(id, constellation, distFromSun, x, y, z, colorIndex, radialVelocity, vx, vy,
+                                          vz);
+                    //cout << star.name << " " << star.constellation << " " << star.distFromSun << " " <<
+                    // star.x << " " << star.y << " " << star.z << " " << star.colorIndex << " " << star.radialVelocity
+                    // << " " << star.vx << " " << star.vy << " " << star.vz << endl;
+                    starCount++;
+                    starSystem.AddStar(star);
+                }
+            }
+            catch (const exception &e) {
+                cerr << "Error parsing line: " << e.what() << endl;
+                continue;
+            }
+        }
+    }
+    file.close();
+
+    starSystem.PrintStarConnections();
 
 
-    g.PrintStarConnections();
-
+    //SFML Window
+    //sf::RenderWindow window(sf::VideoMode(800, 600), "STARS");
+    //https://www.youtube.com/watch?v=BILAqQ-ZFDA
 
     return 0;
 }
